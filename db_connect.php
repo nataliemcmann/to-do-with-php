552530d00f22467741865a5b71edc269e39a09da
Natalie McMann <nmcmann110@gmail.com>
2023-05-05 11:55:22 -0500

    successfully created a dotenv class that retrieves an environmental variable

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,15 @@
 <?php 
+
+    // Load DotEnvironment Class
+    require_once('./dotenv.php');
+    $__DotEnvironment = new DotEnvironment(realpath(__DIR__."/.env"));
+    echo "<pre>";
+    print_r($_ENV);
+    echo "</pre>";
+
+    // connect to postgresql
     try {
-        $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=weekend-to-do-app;user=postgres;password=');
+        $dbh = new PDO(`pgsql:host=localhost;port=5432;dbname=weekend-to-do-app;user=postgres;password=$_ENV`);
         echo "PDO connection object created";
     }
     catch(PDOException $e) {
@@ -8,17 +17,17 @@
     }
 
     //connect to database
-    try {
-        $db = pg_connect("host=localhost port=5432 dbname=weekend-to-do-app");
-        echo `<script type="text/javascript">
-                console.log("db connected");
-        </script>`;
-    } catch(PDOException $e){
-        $error = $e->getMessage();
-        echo `<script type="text/javascript">
-                console.log("cannot connect to db: ${$error}");
-            </script>`;
-    };
+    // try {
+    //     $db = pg_connect("host=localhost port=5432 dbname=weekend-to-do-app");
+    //     echo `<script type="text/javascript">
+    //             console.log("db connected");
+    //     </script>`;
+    // } catch(PDOException $e){
+    //     $error = $e->getMessage();
+    //     echo `<script type="text/javascript">
+    //             console.log("cannot connect to db: ${$error}");
+    //         </script>`;
+    // };
 
     // if (PgSql\Connection) {
     //     $test = pg_query($db, `SELECT * FROM "tasks" ORDER BY "date" ASC;`);
